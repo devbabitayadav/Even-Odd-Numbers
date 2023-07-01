832e75588ec22c8fc907e3db89ef67a1647a4066
“devbabitayadav” <babitayadav9584@gmail.com>
2023-07-01 13:31:28 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const number = prompt("Enter a Number");
+if(number%2==0){
+    document.write(`<h2>The Even Number is: ` + number)
+}
+else{
+    document.write(`<h2>The Odd Number is : ` + number)
+}
